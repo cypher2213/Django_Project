bf187c7af02dd85ed0fa5e3ff2cb7768e8503b30
Nick <melancholic8078@gmail.com>
2025-12-22 19:56:29 +0200

    added images and models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TimeStampsMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+    class Meta:
+        abstract = True
--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from core.models import TimeStampsMixin
 
 # Create your models here.
+
+
+class Product(TimeStampsMixin, models.Model):
+    avatar = models.ImageField(upload_to='products/images', blank=True,null=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True,null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    
+    
+    def __str__(self):
+        return self.name
