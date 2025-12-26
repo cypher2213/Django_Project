5d7dcfb2f1402c2d583fcd88cae164c2450066b8
Nick <melancholic8078@gmail.com>
2025-12-27 01:42:51 +0200

    products section added

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-
+from django.conf import settings
 from .views import home, about
-
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home, name='home'), 
     path('about/',about,name='about')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
