d415552794c0ebba73f70162b0863227c951cca6
Nick <melancholic8078@gmail.com>
2026-01-03 23:14:51 +0200

    fast buttons fix

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home, name='home'), 
-    
     path('login/',login,name='login'),
     path('register/',register,name='register'),
 ]
